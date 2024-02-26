9c043f196fd4f0bd30fd316a4bceeda63c0e0a59
Alessandro Gomes Cunha <agomesc@live.com>
2024-02-26 18:35:46 -0300

    dando continuidade

---
+++ src/Components/main.js
@@ -0,0 +1,13 @@
+// src/components/Main.js
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Seja bem-vindo ao nosso site!</h1>
+      {/* Conteúdo principal aqui */}
+    </div>
+  );
+};
+
+export default Main;
