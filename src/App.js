371dfa10f12b4bf8fb26ac14201c9209d189b6b5
Alessandro Gomes Cunha <agomesc@live.com>
2024-02-29 13:12:37 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import Main from './Components/main';
-import Menu from './Components/menu';
+import React, { useEffect, useState } from "react";
+import Main from "./Components/main";
+import Menu from "./Components/menu";
 import Container from "@mui/material/Container";
-import ImageMasonry  from './Components/ImageMasonry'
-import PhotoGallery from './PhotoGallery'
+import ImageMasonry from "./Components/ImageMasonry";
+import PhotoGallery from "./PhotoGallery";
+import FlickrApp from "./shared/FlickrApp";
 
 const App = () => {
+  const [galleryData, setGalleryData] = useState([]);
+  const apiKey = "099c9a89c04c78ec7592650af1d25a7a";
+
+  useEffect(() => {
+    async function fetchData() {
+      const flickrApp = new FlickrApp(apiKey);
+      const data = await flickrApp.GetGallery();
+      setGalleryData(data);
+    }
+    fetchData();
+
+    console.log("galleryData", galleryData);
+  }, []);
+
   return (
     <Container maxWidth={true} disableGutters>
       <Menu />
