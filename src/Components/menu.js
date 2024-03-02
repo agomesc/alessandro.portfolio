0b98f0476e4447a89e2f8d565e5c195d617510a0
Alessandro Gomes Cunha <agomesc@live.com>
2024-03-01 22:23:40 -0300

    Ajustes para subir a versão em react

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark", // 'light' para tema claro, 'dark' para tema escuro
+    primary: {
+      main: "#000000", // Cor escura
+    },
+    secondary: {
+      main: "#FFFFFF", // Cor clara
+    },
+  },
+});
 
 const Menu = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">Meu Site</Typography>
-        <Button color="inherit" href="/">
-          Início
-        </Button>
-        <Button color="inherit" href="/sobre">
-          Sobre
-        </Button>
-        {/* Adicione mais botões conforme necessário */}
-      </Toolbar>
-    </AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Portfólio</Typography>
+          <Button color="inherit" href="">
+            Início
+          </Button>
+          <Button color="inherit" href="">
+            Sobre
+          </Button>
+          {/* Adicione mais botões conforme necessário */}
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
