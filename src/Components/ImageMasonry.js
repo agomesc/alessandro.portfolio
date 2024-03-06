593ad1f2f3ed3cc8375f6da9a03c7f62787b285b
Alessandro Gomes Cunha <agomesc@live.com>
2024-03-06 16:45:11 -0300

    Ajustes para remover o modal e abrir lateral

--- src/Components/ImageMasonry.js
+++ src/Components/ImageMasonry.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-import { Modal } from "@mui/material";
 import PhotoGallery from "../PhotoGalleryApp";
 import { Box } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Masonry from "@mui/lab/Masonry";
 import { styled } from "@mui/material/styles";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Drawer from '@mui/material/Drawer';
+import MyDrawer from "../Components/MyDrawer";
 
 const Label = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -21,26 +18,25 @@ const Label = styled(Paper)(({ theme }) => ({
 }));
 
 const ImageMasonry = ({ data }) => {
-  const [open, setOpen] = useState(false);
   const [getID, setID] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const handleOpen = (id) => {
-    setID(id);
-    setOpen(true);
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleDrawerOpen = (id) => {
+    setDrawerOpen(true);
+    setID(id);
   };
 
   return (
     <Box sx={{ pt: 4 }}>
-      
       <Typography variant="h4">Minhas Galerias</Typography>
-      
+
       <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2}>
         {data.map((item, index) => (
-          <div key={index} onClick={() => handleOpen(item.id)}>
+          <div key={index} onClick={() => handleDrawerOpen(item.id)}>
             <Label>{item.title}</Label>
             <img
               srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
@@ -60,37 +56,9 @@ const ImageMasonry = ({ data }) => {
         ))}
       </Masonry>
 
-      <Modal
-        sx={{ width: "100%" }}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            bgcolor: "background.paper",
-            boxShadow: 20,
-            display: "flex",
-            width: "90%",
-            height: "90%",
-            overflowY: "scroll",
-            p: 10,
-          }}
-        >
-          <IconButton
-            sx={{ position: "absolute", top: 0, right: 0 }}
-            onClick={handleClose}
-          >
-            <CloseIcon />
-          </IconButton>
-          <PhotoGallery id={getID} />
-        </Box>
-      </Modal>
+      <MyDrawer open={drawerOpen} handleClose={handleDrawerClose}>
+        <PhotoGallery id={getID} />
+      </MyDrawer>
     </Box>
   );
 };
---
+++ src/Components/MyDrawer.js
@@ -0,0 +1,28 @@
+// MyDrawer.js
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+const MyDrawer = ({ open, handleClose, children }) => {
+  return (
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="simple-drawer-title"
+    >
+      <div style={{ width: '100vw', padding: '20px' }}>
+        <IconButton
+          style={{ position: 'absolute', top: 0, right: 0 }}
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+        {children}
+      </div>
+    </Drawer>
+  );
+};
+
+export default MyDrawer;
