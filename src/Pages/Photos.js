dec5d0ffaa2a6f9e9f673ac275ea6023fae1ab54
Alessandro Gomes Cunha <agomesc@live.com>
2024-03-09 12:14:34 -0300

    Ajustes e desenvolvimento

--- src/Pages/Photos.js
+++ src/Pages/Photos.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import FlickrApp from "../shared/FlickrApp";
 import PhotoGallery from "../Components/PhotoGallery";
-import { useParams } from "react-router-dom";
+import { useParams  } from "react-router-dom"; 
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Photos = () => {
   const { id } = useParams();
   const [galleryData, setGalleryData] = useState([]);
   const apiKey = "099c9a89c04c78ec7592650af1d25a7a";
+  
+  const handleGoBack = () => {
+    window.location.href = "/Gallery";    
+  };
+
   useEffect(() => {
     async function fetchData() {
       const flickrApp = new FlickrApp(apiKey);
@@ -16,7 +23,18 @@ const Photos = () => {
     fetchData();
   }, [galleryData, id]);
 
-  return <PhotoGallery photos={galleryData} />;
+  return (
+    <>
+      
+      <IconButton
+        style={{ position: "absolute", top: 0, right: 0 }}
+        onClick={handleGoBack} 
+      >
+        <CloseIcon />
+      </IconButton>
+      <PhotoGallery photos={galleryData} />;
+    </>
+  );
 };
 
 export default Photos;
