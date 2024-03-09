17233a6f66493e1b33bd8aa04455416e9b1c5fb0
Alessandro Gomes Cunha <agomesc@live.com>
2024-03-09 11:49:44 -0300

    Ajustes para usar Rotas

---
+++ src/Pages/Photos.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import FlickrApp from "../shared/FlickrApp";
+import PhotoGallery from "../Components/PhotoGallery";
+import { useParams } from "react-router-dom";
+
+const Photos = () => {
+  const { id } = useParams();
+  const [galleryData, setGalleryData] = useState([]);
+  const apiKey = "099c9a89c04c78ec7592650af1d25a7a";
+  useEffect(() => {
+    async function fetchData() {
+      const flickrApp = new FlickrApp(apiKey);
+      const data = await flickrApp.GetPhotos(id);
+      setGalleryData(data);
+    }
+    fetchData();
+  }, [galleryData, id]);
+
+  return <PhotoGallery photos={galleryData} />;
+};
+
+export default Photos;
