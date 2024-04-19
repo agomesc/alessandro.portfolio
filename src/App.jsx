73dd3d892c0e307ee183655b5e500a99615d5058
Alessandro Gomes Cunha <agomesc@live.com>
2024-04-19 10:56:46 -0300

    Ajustes favicon.png

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const Back = lazy(() => import("./Components/Back"));
 const Footer = lazy(() => import("./Views/Footer"));
 const SocialMetaTags = lazy(() => import("./Components/SocialMetaTags"));
 
-
 const minhaDescricao = `Me chamo Alessandro, 
 		brasileiro, formado em Análise de Sistemas e pós-graduado em Engenharia de Software pela UFRJ, trabalho desde 1994 com tecnologia.
 		Minha história com a fotografia começa mais ou menos assim... Minha mãe sempre fotografou, a mim e a meus irmãos, quando éramos crianças, como forma de guardar nossos momentos da infância e no ano de 2003 eu acabei ganhando a minha primeira máquina digital em um bingo numa festa de trabalho. Desde então, nunca mais me desapeguei da fotografia.
