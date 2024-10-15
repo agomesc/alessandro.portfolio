2341a6ea3da9162aa970a981056767e36f10b805
Alessandro Gomes Cunha <agomesc@live.com>
2024-10-15 19:30:30 -0300

    Ajustes

--- src/Views/LatestPhotos.jsx
+++ src/Views/LatestPhotos.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState, Suspense, lazy } from "react";
 import CreateFlickrApp from "../shared/CreateFlickrApp";
 import LoadingMessage from "../Components/LoadingMessage";
-import logo from "../images/logo_192.png"
+import logo from "../images/logo_192.png";
 import SocialMetaTags from "../Components/SocialMetaTags";
 
 const PhotoGrid = lazy(() => import("../Components/PhotoGrid"));
 
 const LatestPhotos = () => {
-
 	const [galleryData, setGalleryData] = useState(null);
 	const instance = CreateFlickrApp();
 
@@ -16,17 +15,17 @@ const LatestPhotos = () => {
 			const data = await instance.getLatestPhotos();
 			setGalleryData(data);
 		}
-		if (!galleryData) fetchData();
-	}, [galleryData, instance]);
+		fetchData();
+	}, [instance]);
 
 	const title = 'Atualizações';
 	const description = 'Últimas Atualizações';
 
 	return (
 		<>
-			<SocialMetaTags title={title} url={window.location.href} description={description} imageUrl={logo} />
+			<SocialMetaTags title={title} url={logo} description={description} />
 			<Suspense fallback={<LoadingMessage />}>
-				<PhotoGrid itemData={galleryData} />
+				{galleryData ? <PhotoGrid itemData={galleryData} /> : <LoadingMessage />}
 			</Suspense>
 		</>
 	);
