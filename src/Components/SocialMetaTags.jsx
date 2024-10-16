7a65109cf0afcf7b4a2abf2c2af94adc0edd837c
Alessandro Gomes Cunha <agomesc@live.com>
2024-10-16 09:24:53 -0300

    Ajustes

--- src/Components/SocialMetaTags.jsx
+++ src/Components/SocialMetaTags.jsx
@@ -8,33 +8,62 @@ const SocialMetaTags = ({ title, description, url }) => {
   useEffect(() => {
     const handleDomLoaded = () => {
       setCurrentUrl(window.location.href);
-      setIsDomLoaded(true); // DOM está carregada
+      setIsDomLoaded(true);
     };
 
     if (document.readyState === 'complete') {
-      handleDomLoaded(); // DOM já estava carregada
+      handleDomLoaded();
     } else {
-      window.addEventListener('load', handleDomLoaded); // Aguardando a DOM carregar
+      window.addEventListener('load', handleDomLoaded);
     }
 
-    return () => window.removeEventListener('load', handleDomLoaded); // Limpeza
+    return () => window.removeEventListener('load', handleDomLoaded);
   }, []);
 
-  if (!isDomLoaded) return null; // Aguarda o DOM estar carregado antes de renderizar o componente
+  useEffect(() => {
+    if (isDomLoaded) {
+      if (title) document.title = title;
+
+      if (description) {
+        let descMeta = document.querySelector("meta[name='description']");
+        if (!descMeta) {
+          descMeta = document.createElement('meta');
+          descMeta.name = 'description';
+          document.head.appendChild(descMeta);
+        }
+        descMeta.setAttribute('content', description);
+      }
+
+      if (url) {
+        let ogImageMeta = document.querySelector("meta[property='og:image']");
+        if (!ogImageMeta) {
+          ogImageMeta = document.createElement('meta');
+          ogImageMeta.setAttribute('property', 'og:image');
+          document.head.appendChild(ogImageMeta);
+        }
+        ogImageMeta.setAttribute('content', url);
+
+        let twitterImageMeta = document.querySelector("meta[name='twitter:image']");
+        if (!twitterImageMeta) {
+          twitterImageMeta = document.createElement('meta');
+          twitterImageMeta.name = 'twitter:image';
+          document.head.appendChild(twitterImageMeta);
+        }
+        twitterImageMeta.setAttribute('content', url);
+      }
+    }
+  }, [isDomLoaded, title, description, url]);
+
+  if (!isDomLoaded) return null;
 
   return (
     <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
       <meta property="og:url" content={currentUrl} />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={url} />
-
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={url} />
     </Helmet>
   );
 };
