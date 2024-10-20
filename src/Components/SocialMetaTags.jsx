40c379223fd8c88fa7222252096ffb91eccca69c
Alessandro Gomes Cunha <agomesc@live.com>
2024-10-20 16:15:27 -0300

    Ajustes

--- src/Components/SocialMetaTags.jsx
+++ src/Components/SocialMetaTags.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const SocialMetaTags = ({ title, description, url }) => {
   const [metaTitle, setMetaTitle] = useState(title);
