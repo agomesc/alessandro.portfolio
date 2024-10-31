d36614ff7177f414037f5e6cb5eb537b3cc16694
Alessandro Gomes Cunha <agomesc@live.com>
2024-10-31 08:44:20 -0300

    Ajustes

--- src/shared/CreateFetchService.js
+++ src/shared/CreateFetchService.js
@@ -3,29 +3,24 @@ function CreateFetchService() {
 		try {
 			const response = await fetch(url, options);
 			if (!response.ok) {
-				throw new Error(
-					`Erro na requisição: ${response.status} ${response.statusText}`
-				);
+				throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`);
 			}
-			return response.json();
+			return await response.json(); // Use await here
 		} catch (error) {
-			throw new Error(TryError(error.status));
+			throw new Error(TryError(error.message));
 		}
 	}
 
 	async function get(url) {
-		return fetchWithInterceptor(url);
+		return await fetchWithInterceptor(url); // Use await here
 	}
 
 	async function post(url, data) {
-
 		const cacheKey = `${url}-${JSON.stringify(data)}`;
 		const cachedResponse = sessionStorage.getItem(cacheKey);
-
 		if (cachedResponse) {
 			return JSON.parse(cachedResponse);
 		}
-
 		const options = {
 			method: "POST",
 			headers: {
@@ -35,50 +30,48 @@ function CreateFetchService() {
 			},
 			body: JSON.stringify(data),
 		};
-		const response = await fetchWithInterceptor(url, options);
+		const response = await fetchWithInterceptor(url, options); // Use await here
 		sessionStorage.setItem(cacheKey, JSON.stringify(response));
 		return response;
 	}
-	return {
-		get,
-		post,
-	};
+
+	return { get, post };
 }
 
-function TryError(status) {
-	switch (status) {
-		case 201:
+function TryError(message) {
+	switch (message) {
+		case "201":
 			return "A solicitação foi bem-sucedida, mas, o servidor criou um novo recurso.";
-		case 202:
+		case "202":
 			return "O servidor aceitou a solicitação, mas não a processou a tempos.";
-		case 203:
+		case "203":
 			return "O servidor processou a solicitação com sucesso, mas está retornando informações que podem ser de outra fonte.";
-		case 204:
+		case "204":
 			return "O servidor processou a solicitação com sucesso, mas não está retornando conteúdo algum.";
-		case 205:
+		case "205":
 			return "O servidor processou a solicitação com sucesso, mas não está retornando conteúdo algum porque o conteúdo está sendo redefinido.";
-		case 206:
+		case "206":
 			return "O servidor processou uma solicitação parcialmente.";
-		case 400:
+		case "400":
 			return "O servidor não entendeu o código da solicitação.";
-		case 401:
+		case "401":
 			return "Não Autorizado.";
-		case 403:
+		case "403":
 			return "O servidor recusou a solicitação.";
-		case 404:
+		case "404":
 			return "O servidor não encontrou a página solicitada.";
-		case 405:
+		case "405":
 			return "MethodNotAllowed.";
-		case 408:
+		case "408":
 			return "O servidor expirou ao esperar pela solicitação.";
-		case 500:
+		case "500":
 			return "Erro 500 (Erro interno do servidor).";
-		case 501:
+		case "501":
 			return "O servidor não tem a funcionalidade necessária para completar a solicitação.";
-		case 503:
+		case "503":
 			return "O servidor está indisponível.";
 		default:
-			return "O servidor está indisponível.";
+			return "Erro desconhecido.";
 	}
 }
 
