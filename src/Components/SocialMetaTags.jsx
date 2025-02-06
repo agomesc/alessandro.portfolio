d35af1579a5ab97e1a844c77f981671196356a4e
Alessandro Gomes Cunha <agomesc@live.com>
2025-02-06 13:26:55 -0300

    Ajustes

--- src/Components/SocialMetaTags.jsx
+++ src/Components/SocialMetaTags.jsx
@@ -1,48 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const SocialMetaTags = ({ defaultTitle, defaultDescription, defaultImageUrl, defaultUrl }) => {
-  useEffect(() => {
-    // Pegando valores do sessionStorage ou usando os padrões fornecidos
-    const title = sessionStorage.getItem('ogTitle') || defaultTitle;
-    const description = sessionStorage.getItem('ogDescription') || defaultDescription;
-    const imageUrl = sessionStorage.getItem('ogImage') || defaultImageUrl;
-    const url = sessionStorage.getItem('ogUrl') || defaultUrl;
+const SocialMetaTags = ({ title, description, url }) => {
 
-    // Atualizando sessionStorage
-    sessionStorage.setItem('ogTitle', title);
-    sessionStorage.setItem('ogDescription', description);
-    sessionStorage.setItem('ogImage', imageUrl);
-    sessionStorage.setItem('ogUrl', url);
-
-    // Atualizando meta tags dinamicamente
-    const updateMetaTag = (selector, content) => {
-      let tag = document.querySelector(selector);
-      if (!tag) {
-        tag = document.createElement('meta');
-        if (selector.includes('property')) {
-          tag.setAttribute('property', selector.split('=')[1].replace(/"/g, ''));
-        } else {
-          tag.setAttribute('name', selector.split('=')[1].replace(/"/g, ''));
-        }
-        document.head.appendChild(tag);
-      }
-      tag.setAttribute('content', content);
-    };
-
-    updateMetaTag('meta[name="description"]', description);
-    updateMetaTag('meta[property="og:title"]', title);
-    updateMetaTag('meta[property="og:description"]', description);
-    updateMetaTag('meta[property="og:image"]', imageUrl);
-    updateMetaTag('meta[property="og:url"]', url);
-    updateMetaTag('meta[name="twitter:title"]', title);
-    updateMetaTag('meta[name="twitter:description"]', description);
-    updateMetaTag('meta[name="twitter:image"]', imageUrl);
-
-    // Atualizando o título da página
-    document.title = title;
-  }, [defaultTitle, defaultDescription, defaultImageUrl, defaultUrl]);
-
-  return null; // Não renderiza nada no DOM
+  return (
+    <>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={url} />
+        <meta property="og:url" content={url} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={url} />
+    </>
+  );
 };
 
 export default React.memo(SocialMetaTags);
