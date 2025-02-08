768c3759069cdb3fd143e5df761845533c2db8e2
Alessandro Gomes Cunha <agomesc@live.com>
2025-02-08 10:36:08 -0300

    Ajustes

--- src/Components/PhotoDescription.jsx
+++ src/Components/PhotoDescription.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
+import ImageComponent from './ImageComponent'
+
 
 const PhotoDescription = ({ imageUrl, description }) => {
+
+  const autor = 'Alessandro Gomes';
+
   return (
     <Box
       sx={{
         p: 0,
-        width: "98%",
+        width: "100%",
         alignContent: "center",
         alignItems: "center",
         margin: "0 auto",
@@ -20,50 +21,16 @@ const PhotoDescription = ({ imageUrl, description }) => {
       <Typography sx={{ mt: 10, mb: 3 }} variant="h3">
         Sobre?
       </Typography>
-      <Paper elevation={3}>
-        <Grid container justifyContent="center">
-          <Grid item>
-            <Card
-              style={{
-                maxWidth: "50%",
-                margin: "0 auto",
-              }}
-            >
-              <CardContent 
-                sx={{ 
-                  display: 'flex', 
-                  flexDirection: 'column', 
-                  alignItems: 'center' 
-                }}
-              >
-                <Box
-                  component="img"
-                  sx={{
-                    width: "10%",
-                    height: "auto",
-                  }}
-                  src={imageUrl}
-                  alt="Minha Imagem"
-                />
-                <Paper
-                  elevation={1}
-                  sx={{
-                    p: 2,
-                    mt: 2,
-                    width: '100%'
-                  }}
-                >
-                  <Typography 
-                    variant="body1" component="p"
-                  >
-                    {description}
-                  </Typography>
-                </Paper>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </Paper>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+        <ImageComponent src={imageUrl} alt={autor} />
+      </Box>
+
+
+      <Typography variant="body1" component="p" sx={{ mt: 2, textAlign: "left" }}>
+        {description}
+      </Typography>
+
     </Box>
   );
 };
