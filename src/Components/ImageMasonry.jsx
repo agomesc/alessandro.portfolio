70f4295ad4dca5484d0714f0bb3af165dbc72da4
Alessandro Gomes Cunha <agomesc@live.com>
2025-03-11 18:28:06 -0300

    Ajustes

--- src/Components/ImageMasonry.jsx
+++ src/Components/ImageMasonry.jsx
@@ -4,7 +4,7 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { Masonry } from "@mui/lab";
 import { styled } from "@mui/material/styles";
-  import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ImageComponent from './ImageComponent';
 import useMediaQuery from "@mui/material/useMediaQuery";
 
@@ -93,7 +93,7 @@ const ImageMasonry = ({ data }) => {
           ))}
         </>
       ) : (
-        <Masonry columns={{ xs: 2, sm: 3, md: 4, lg: 5 }} spacing={1}>
+        <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={1}>
           {data.map((item, index) => (
             <GalleryContainer key={index}>
               <NavLink key={index} to={`/Photos/${item.id}`}>
--- src/Components/PhotoGallery.jsx
+++ src/Components/PhotoGallery.jsx
@@ -41,7 +41,7 @@ const PhotoGallery = ({ photos }) => {
   return (
     <>
       {!showModal && (
-        <Masonry columns={{ xs: 2, sm: 3, md: 4, lg: 5 }} spacing={1}>
+        <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={1}>
           {photos.map((item, index) => (
             <GalleryContainer
               key={index}
