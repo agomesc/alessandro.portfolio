93f9468e3edb7bb4a7638fff8c12c0d7ba964b58
Alessandro Gomes Cunha <agomesc@live.com>
2025-03-18 09:20:59 -0300

    Ajustes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const functions = require('firebase-functions');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -5,27 +6,26 @@ const path = require('path');
 const app = express();
 const buildPath = path.join(__dirname, 'build');
 
-// Não defina o express.static antes do código que modifica o HTML
-app.get('/', (req, res) => {
-    const indexPath = path.join(buildPath, 'index.html');
-
-    fs.readFile(indexPath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Erro ao carregar o index.html');
-        }
-
-        // Modifica dinamicamente a meta tag description
-        const modifiedHtml = data.replace(
-            /<meta name="description" content=".*?">/,
-            '<title>Alterado com sucsso!</title>',
-            '<meta name="description" content="Descrição dinâmica via Express">'
-        );
-
-        res.send(modifiedHtml);
-    });
-});
-
-// Agora defina express.static após o código de modificação
+// Servir arquivos estáticos
 app.use(express.static(buildPath));
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+app.get('*', (req, res) => {
+  const indexPath = path.join(buildPath, 'index.html');
+
+  fs.readFile(indexPath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Erro ao carregar o index.html');
+    }
+
+    // Modifica dinamicamente a meta tag description
+    const modifiedHtml = data.replace(
+      /<meta name="description" content=".*?">/,
+      '<meta name="description" content="Descrição dinâmica via Express">'
+    );
+
+    res.send(modifiedHtml);
+  });
+});
+
+// Exporta a função para o Firebase Functions
+exports.app = functions.https.onRequest(app);
--- src/Components/SocialMetaTags.jsx
+++ src/Components/SocialMetaTags.jsx
@@ -1,33 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
+import React from "react";
 
 // https://metatags.io/
 const SocialMetaTags = ({ title, image, description }) => {
-  const [tituloAtual, setTituloAtual] = useState('');
-  const [imagemAtual, setImagemAtual] = useState('');
-  const [descricaoAtual, setdescricaoAtual] = useState('');
-  const [urlAtual, setUrlAtual] = useState('');
-
-  useEffect(() => {
-    setUrlAtual(window.location.href);
-    setTituloAtual(title);
-    setImagemAtual(image);
-    setdescricaoAtual(description);
-  }, [description, image, title]);
-
   return (
     <>
-      <Helmet>
-        <title>{title}</title>
-        <meta property="og:title" content={tituloAtual} />
-        <meta property="og:description" content={descricaoAtual} />
-        <meta property="og:image" content={imagemAtual || urlAtual} />
-        <meta property="og:url" content={urlAtual} />
-        <meta name="twitter:title" content={tituloAtual} />
-        <meta name="twitter:description" content={descricaoAtual} />
-        <meta name="twitter:image" content={imagemAtual || urlAtual} />
-        <meta name="twitter:site" content="@olhotofografico" />
-      </Helmet>
     </>
   );
 };
