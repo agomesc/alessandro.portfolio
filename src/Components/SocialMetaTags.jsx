3a03556fbfd82389b6f58777caf1f8442935a323
Alessandro Gomes Cunha <agomesc@live.com>
2025-03-18 16:57:52 -0300

    Ajustes

--- src/Components/SocialMetaTags.jsx
+++ src/Components/SocialMetaTags.jsx
@@ -1,34 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
 
-// https://metatags.io/
 const SocialMetaTags = ({ title, image, description }) => {
-  const [tituloAtual, setTituloAtual] = useState('');
-  const [imagemAtual, setImagemAtual] = useState('');
-  const [descricaoAtual, setdescricaoAtual] = useState('');
-  const [urlAtual, setUrlAtual] = useState('');
-
-  useEffect(() => {
-    setUrlAtual(window.location.href);
-    setTituloAtual(title);
-    setImagemAtual(image);
-    setdescricaoAtual(description);
-  }, [description, image, title]);
+  const urlAtual = typeof window !== "undefined" ? window.location.href : "";
 
   return (
-    <>
-      <Helmet>
-        <title>{title}</title>
-        <meta property="og:title" content={tituloAtual} />
-        <meta property="og:description" content={descricaoAtual} />
-        <meta property="og:image" content={imagemAtual || urlAtual} />
-        <meta property="og:url" content={urlAtual} />
-        <meta name="twitter:title" content={tituloAtual} />
-        <meta name="twitter:description" content={descricaoAtual} />
-        <meta name="twitter:image" content={imagemAtual || urlAtual} />
-        <meta name="twitter:site" content="@olhotofografico" />
-      </Helmet>
-    </>
+    <Helmet>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image || urlAtual} />
+      <meta property="og:url" content={urlAtual} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image || urlAtual} />
+      <meta name="twitter:site" content="@olhotofografico" />
+    </Helmet>
   );
 };
 
