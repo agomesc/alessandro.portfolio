19771851855e54e482a6b258f7eb3604a8b8028f
Alessandro Gomes Cunha <agomesc@live.com>
2025-03-19 10:36:30 -0300

    Ajustes

--- src/Components/ImageComponent.jsx
+++ src/Components/ImageComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ImageComponent = ({ src, alt, width, height }) => (
   <img
@@ -8,10 +9,22 @@ const ImageComponent = ({ src, alt, width, height }) => (
     style={{
       width: width,
       height: height,
-      objectFit: "cover",
-      display: "flex"
+      objectFit: 'cover',
+      display: 'flex',
     }}
   />
 );
+
+ImageComponent.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+ImageComponent.defaultProps = {
+  width: '100%',
+  height: 'auto',
+};
 
 export default React.memo(ImageComponent);
