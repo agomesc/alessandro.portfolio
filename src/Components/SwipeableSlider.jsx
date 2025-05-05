aa3e5282d4552648c8b167a49fb73510f364529a
Alessandro Gomes Cunha <agomesc@live.com>
2025-05-05 17:42:07 -0300

    Ajustes

--- src/Components/SwipeableSlider.jsx
+++ src/Components/SwipeableSlider.jsx
@@ -2,27 +2,24 @@ import React from 'react';
 import Box from "@mui/material/Box";
 import TypographyTitle from './TypographyTitle';
 import PhotoCarousel from './PhotoCarousel'; // já deve estar implementado
-import { useSwipeable } from 'react-swipeable';
 
 const SwipeableSlider = ({ itemData }) => {
-  const handlers = useSwipeable({
-    onSwipedLeft: () => console.log('Swiped Left'),
-    onSwipedRight: () => console.log('Swiped Right'),
-  });
-
   return (
     <Box
-      {...handlers}
       sx={{
+        p: 0,
         width: {
-          xs: "100%",
-          sm: "90%",
-          md: "80%",
-          lg: "70%",
-          xl: "80%",
+          xs: "100%", // Para telas extra pequenas (mobile)
+          sm: "90%",  // Para telas pequenas
+          md: "80%",  // Para telas médias
+          lg: "70%",  // Para telas grandes
+          xl: "80%"   // Para telas extra grandes
         },
+        alignContent: "center",
+        alignItems: "center",
         margin: "0 auto",
         padding: "0 20px",
+        mt: 10
       }}
     >
       <TypographyTitle src="Atualizações" />
