742e73627104b08709dc3b844e70f807e36a73c4
Alessandro Gomes Cunha <50428053+agomesc@users.noreply.github.com>
2025-05-16 15:42:37 -0300

    Ajustes

--- src/Components/PhotoCarousel.jsx
+++ src/Components/PhotoCarousel.jsx
@@ -1,9 +1,8 @@
 import React, { lazy, Suspense } from "react";
 import "react-image-gallery/styles/css/image-gallery.css";
-import Skeleton from '@mui/material/Skeleton';
+import Skeleton from "@mui/material/Skeleton";
 import LazyImage from "../Components/LazyImage";
-
-const ImageGallery = lazy(() => import("react-image-gallery"));
+import ImageGallery from "react-image-gallery";
 
 const PhotoCarousel = ({ photos }) => {
   const galleryImages = photos.map((item) => ({
@@ -12,26 +11,25 @@ const PhotoCarousel = ({ photos }) => {
   }));
 
   const renderItem = (item) => (
-  
-      <LazyImage
-        src={item.original}
-        alt={item.title || "Imagem da prévia"}
-        width={1200}
-        height={720}
-      />
-
-  
+    <LazyImage
+      src={item.original}
+      alt={item.title || "Imagem da prévia"}
+      width={1200}
+      height={720}
+    />
   );
 
   return (
-    <Suspense fallback={<Skeleton variant="rectangular" height={400} />}>
-      <ImageGallery
-        items={galleryImages}
-        showPlayButton={false}
-        showFullscreenButton={false}
-        renderItem={renderItem}
-      />
-    </Suspense>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%" }}>
+      <Suspense fallback={<Skeleton variant="rectangular" />}>
+        <ImageGallery
+          items={galleryImages}
+          showPlayButton={false}
+          showFullscreenButton={false}
+          renderItem={renderItem}
+        />
+      </Suspense>
+    </div>
   );
 };
 
